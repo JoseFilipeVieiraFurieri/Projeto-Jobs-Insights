d94402da06fa542fe77ba5e7730909d9b2f55687
JoseFilipeVieiraFurieri <jf.furieri@ymail.com>
2023-04-27 20:47:04 -0300

    feat: requisito 5

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -15,21 +15,15 @@ def get_max_salary(path: str) -> int:
 
 
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
+    job_data = read(path)
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    return min(
+        [
+            int(job["min_salary"])
+            for job in job_data
+            if job["min_salary"].isdigit()
+        ]
+    )
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
