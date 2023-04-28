c0b71f41eebf6ac7dc14528d82ff5baf7bbd78cd
JoseFilipeVieiraFurieri <jf.furieri@ymail.com>
2023-04-28 19:08:07 -0300

    feat: requisito 12

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,7 +1,17 @@
-# from src.pre_built.sorting import sort_by
-# from src.insights.jobs import read
+from src.pre_built.sorting import sort_by
 
 
 def test_sort_by_criteria():
+    mock_data = [
+        {"min_salary": 3000, "max_salary": 5000, "date_posted": "25-06-22"},
+        {"min_salary": 5000, "max_salary": 10000, "date_posted": "25-06-22"},
+    ]
 
-    pass
+    expect_result = [
+        {"min_salary": 5000, "max_salary": 10000, "date_posted": "25-06-22"},
+        {"min_salary": 3000, "max_salary": 5000, "date_posted": "25-06-22"},
+    ]
+
+    sort_by(mock_data, "max_salary")
+
+    assert mock_data == expect_result
